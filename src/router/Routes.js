02b24c20f5014d0349be90045de060db509f8378
Jack Perry <jack.e.perry@outlook.com>
2021-03-22 15:49:26 -0600

    👌 IMPROVE: scrollToTop component added and basic Routes structured formed

---
+++ src/router/Routes.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+import { Route } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop";
+
+export default () => {
+  return (
+    <Fragment>
+      <ScrollToTop />
+    </Fragment>
+  );
+};
