fab53a3b3bf2dd83c5e03666b27f6f016c0a886f
Jack Perry <jack.e.perry@outlook.com>
2021-03-23 07:53:47 -0600

    👌 IMPROVE: routes set up, default page foundations set, ready for merge

--- src/router/Routes.js
+++ src/router/Routes.js
@@ -1,17 +1,17 @@
 import React, { Fragment } from 'react';
 import { Route } from 'react-router-dom';
 import ScrollToTop from './ScrollToTop';
-import About from '../pages/About';
-import Blogposts from '../pages/Blogposts';
-import Contact from '../pages/Contact';
-import Home from '../pages/Home';
-import Projects from '../pages/Projects';
+import About from '../pages/About/About';
+import Blogposts from '../pages/Blogposts/Blogposts';
+import Contact from '../pages/Contact/Contact';
+import Home from '../pages/Home/Home';
+import Projects from '../pages/Projects/Projects';
 
 export default () => {
   return (
     <Fragment>
       <ScrollToTop />
-      <Route exact path='/' Component={Home} />
+      <Route exact path='/' component={Home} />
       <Route exact path='/about' component={About} />
       <Route exact path='/blogposts' component={Blogposts} />
       <Route exact path='/contact' component={Contact} />
