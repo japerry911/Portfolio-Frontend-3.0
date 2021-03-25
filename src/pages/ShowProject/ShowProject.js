85f52e2e751ef34778717a5ec75c8f9a5949cc89
Jack Perry <jack.e.perry@outlook.com>
2021-03-25 17:05:12 -0600

    📦 NEW: started, routing added

---
+++ src/pages/ShowProject/ShowProject.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useStyles } from './ShowProjectStyles';
+
+const ShowProject = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <h1>WOOHOO</h1>
+    </div>
+  );
+};
+
+export default ShowProject;
