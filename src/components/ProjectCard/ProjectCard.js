3b98641d684282533f470a695ec34a5d62bb782f
Jack Perry <jack.e.perry@outlook.com>
2021-03-25 16:51:49 -0600

    👌 IMPROVE: finished draft of projects page, ready-for-merge

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,4 +1,10 @@
 import React, { Fragment } from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
 import { useStyles } from './ProjectCardStyles';
 
 const ProjectCard = ({ projectObject }) => {
@@ -6,7 +12,32 @@ const ProjectCard = ({ projectObject }) => {
 
   return (
     <Fragment>
-      <span>test</span>
+      <Card className={classes.cardStyle}>
+        <CardActionArea
+          component={Link}
+          to={`/projects/${projectObject._id}`}
+          className={classes.cardActionAreaStyle}
+        >
+          <CardMedia
+            className={classes.mediaStyle}
+            component='img'
+            alt={`${projectObject.title}-Preview-Image`}
+            height='50%'
+            image={projectObject.imgUrl}
+            title={projectObject.title}
+          />
+          <CardContent>
+            <Typography
+              gutterBottom
+              variant='h5'
+              component='h2'
+              className={classes.fontStyle}
+            >
+              {projectObject.title}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
     </Fragment>
   );
 };
--- src/components/ProjectCard/ProjectCardStyles.js
+++ src/components/ProjectCard/ProjectCardStyles.js
@@ -1,3 +1,28 @@
 import makeStyles from '@material-ui/core/styles/makeStyles';
 
-export const useStyles = makeStyles((theme) => ({}));
+export const useStyles = makeStyles((theme) => ({
+  cardActionAreaStyle: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+  mediaStyle: {
+    objectFit: 'contain',
+    paddingTop: '3rem',
+  },
+  cardStyle: {
+    margin: '3rem',
+    height: '25rem',
+    width: '25%',
+    opacity: 0.75,
+    transition: '350ms ease-in',
+    '&:hover': {
+      opacity: 1,
+    },
+  },
+  fontStyle: {
+    fontFamily: theme.fonts.avenir,
+    textAlign: 'center',
+  },
+}));
