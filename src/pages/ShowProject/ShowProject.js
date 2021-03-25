9c2ec4d7d101b5a7f10f1276384349f17e7cb5e1
Jack Perry <jack.e.perry@outlook.com>
2021-03-25 17:11:33 -0600

    👌 IMPROVE: useEffect set up

--- src/pages/ShowProject/ShowProject.js
+++ src/pages/ShowProject/ShowProject.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import expressServer from '../../api/expressServer';
 import { useStyles } from './ShowProjectStyles';
 
 const ShowProject = () => {
   const classes = useStyles();
 
+  const [isLoading, setIsLoading] = useState(true);
+  const [project, setProject] = useState({});
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    const fetchData = async () => {
+      const projectResult = await expressServer.get(`/projects/${params.id}`);
+
+      setProject(projectResult.data);
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  const params = useParams();
+
   return (
     <div>
-      <h1>WOOHOO</h1>
+      <h1>WOOHOO - {project.title}</h1>
     </div>
   );
 };
