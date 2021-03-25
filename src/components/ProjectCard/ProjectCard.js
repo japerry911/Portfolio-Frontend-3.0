0d67ecb29ba50fc17e5c3e8de6c1201d5e06b3fa
Jack Perry <jack.e.perry@outlook.com>
2021-03-25 16:32:08 -0600

    👌 IMPROVE: basic outline of projectCard

---
+++ src/components/ProjectCard/ProjectCard.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import { useStyles } from './ProjectCardStyles';
+
+const ProjectCard = ({ projectObject }) => {
+  const classes = useStyles();
+
+  return (
+    <Fragment>
+      <span>test</span>
+    </Fragment>
+  );
+};
+
+export default ProjectCard;
