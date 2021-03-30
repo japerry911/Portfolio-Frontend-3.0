6b771395a335f443fd48b82618ef17cb29ad7d31
Jack Perry <jack.e.perry@outlook.com>
2021-03-29 22:07:34 -0600

    👌 IMPROVE: bedtime

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
 import { useStyles } from './ContactStyles';
 
 const Contact = () => {
@@ -7,7 +9,39 @@ const Contact = () => {
 
   return (
     <div className={classes.mainDivStyle}>
-      <Grid container className={classes.mainGridContainerStyle}></Grid>
+      <Grid
+        container
+        style={{ minHeight: '40rem' }}
+        direction='column'
+        alignItems='center'
+      >
+        <Paper className={classes.mainPaperStyle}>
+          <form className={classes.formStyle}>
+            <TextField
+              label='Name'
+              className={classes.textFieldStyle}
+              required
+            />
+            <TextField
+              label='Email'
+              className={classes.textFieldStyle}
+              required
+            />
+            <TextField
+              label='Subject'
+              className={classes.textFieldStyle}
+              required
+            />
+            <TextField
+              label='Message'
+              className={classes.textFieldStyle}
+              required
+              variant='filled'
+              color='primary'
+            />
+          </form>
+        </Paper>
+      </Grid>
     </div>
   );
 };
--- src/pages/Contact/ContactStyles.js
+++ src/pages/Contact/ContactStyles.js
@@ -4,15 +4,36 @@ export const useStyles = makeStyles((theme) => ({
   mainDivStyle: {
     flexGrow: 1,
     minHeight: '100vh',
-  },
-  mainGridContainerStyle: {
     backgroundImage:
       'linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(https://s3.us-east-2.amazonaws.com/portfolio-bucket-3.0-29134u89324809184/contact/benjamin-voros-phIFdC6lA4E-unsplash.jpg)',
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
     backgroundPosition: '0 100%',
-    minHeight: '100vh',
     backgroundAttachment: 'fixed',
-    paddingTop: '10rem',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  mainPaperStyle: {
+    padding: '3rem 0',
+    margin: '10rem 0',
+    width: '65rem',
+    minHeight: '40rem',
+    backgroundColor: 'rgba(93,112,146,.50)',
+  },
+  formStyle: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    minHeight: '35rem',
+  },
+  textFieldStyle: {
+    width: '50%',
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: 'red',
+      },
+    },
   },
 }));
