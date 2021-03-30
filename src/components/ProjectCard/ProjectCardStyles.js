1f07a8899b7ef5611ab7cbde8313a3c4faebc409
Jack Perry <jack.e.perry@outlook.com>
2021-03-30 12:15:51 -0600

    🐛 FIX: fixed width for project cards

--- src/components/ProjectCard/ProjectCardStyles.js
+++ src/components/ProjectCard/ProjectCardStyles.js
@@ -14,7 +14,7 @@ export const useStyles = makeStyles((theme) => ({
   cardStyle: {
     margin: '3rem',
     height: '25rem',
-    width: '25%',
+    width: '35%',
     opacity: 0.75,
     transition: '350ms ease-in',
     '&:hover': {
