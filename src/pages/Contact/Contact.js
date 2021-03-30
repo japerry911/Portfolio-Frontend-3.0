986060251114b78506e691d3975edf4a68dd151a
Jack Perry <jack.e.perry@outlook.com>
2021-03-29 21:50:55 -0600

    📦 NEW: foundation set

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 import { useStyles } from './ContactStyles';
 
 const Contact = () => {
   const classes = useStyles();
 
   return (
-    <div>
-      <h1>CONTACT</h1>
+    <div className={classes.mainDivStyle}>
+      <Grid container className={classes.mainGridContainerStyle}></Grid>
     </div>
   );
 };
--- src/pages/Contact/ContactStyles.js
+++ src/pages/Contact/ContactStyles.js
@@ -1,3 +1,18 @@
 import makeStyles from '@material-ui/core/styles/makeStyles';
 
-export const useStyles = makeStyles((theme) => ({}));
+export const useStyles = makeStyles((theme) => ({
+  mainDivStyle: {
+    flexGrow: 1,
+    minHeight: '100vh',
+  },
+  mainGridContainerStyle: {
+    backgroundImage:
+      'linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(https://s3.us-east-2.amazonaws.com/portfolio-bucket-3.0-29134u89324809184/contact/benjamin-voros-phIFdC6lA4E-unsplash.jpg)',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: '0 100%',
+    minHeight: '100vh',
+    backgroundAttachment: 'fixed',
+    paddingTop: '10rem',
+  },
+}));
